2921667ceac59a4b8ca0ec8b6f0cfb8a370f8c0b
SchrodingerZhu <i@zhuyi.fan>
2021-03-08 19:17:47 +0800

    add module

---
+++ language/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module fan.zhuyi.selfish.language {
+    requires org.graalvm.truffle;
+    requires java.desktop;
+
+    provides com.oracle.truffle.api.TruffleLanguage.Provider
+            with fan.zhuyi.selfish.language.SelfishLanguageProvider;
+
+    exports fan.zhuyi.selfish.language;
+    exports fan.zhuyi.selfish.language.runtime;
+    exports fan.zhuyi.selfish.language.syntax;
+    exports fan.zhuyi.selfish.language.utils;
+    exports fan.zhuyi.selfish.language.node;
+}
